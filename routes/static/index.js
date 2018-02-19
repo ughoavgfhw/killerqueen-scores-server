9691f7b6ff1f7b9c7ccfcb800fecd96933d893ed
ughoavgfhw <ughoavgfhw@gmail.com>
2018-02-19 15:26:27 -0600

    Add team name dropdowns based on a text file

--- routes/static/index.js
+++ routes/static/index.js
@@ -1,8 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
+var fs = require('fs');
+var teams = null;
+if (fs.existsSync('assets/teamNames.txt')) {
+    teams = fs.readFileSync('assets/teamNames.txt', 'utf8').split('\n');
+    if (teams.length > 0 && teams[teams.length - 1] == '') {
+        teams.pop()
+    }
+    console.log('Loaded ' + teams.length + ' team names');
+    if (teams.length == 0) teams = null;
+} else {
+    console.log('No team names: assets/teamNames.txt does not exist');
+}
+
 router.get('/', function(req, res) {
-    res.render('index.html');
+    res.render('index.html', {'teamNames': teams});
 });
 
 module.exports = router;
